83f73fa7fd7e28dd6ee1c0b90198b53e20693f15
milkyway20021012 <milkyway20021012@gmail.com>
2025-02-27 14:04:37 +0800

    Update main1.c

--- qz1/main1.c
+++ qz1/main1.c
@@ -8,13 +8,13 @@ int main()
     printf("70元\n");
   }
   else if( additional <= 100){
-  printf("80元\n");
+    printf("80元\n");
   }
   else{
-  int count = additional / 100;
-  int remainder = (additional % 100 )?1:0;
-  int sum = (count + remainder ) * 10 + 70;
-  printf("%d元\n", sum);
+    int count = additional / 100;
+    int remainder = (additional % 100 )?1:0;
+    int sum = (count + remainder ) * 10 + 70;
+    printf("%d元\n", sum);
   }
   return 0;
 }
