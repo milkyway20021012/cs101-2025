63a3feae2d73afb54d67386248ff306378bf011b
milkyway20021012 <milkyway20021012@gmail.com>
2025-03-13 16:00:59 +0800

    ex2

--- ex2/main.c
+++ ex2/main.c
@@ -3,18 +3,18 @@
 
 typedef struct employee
 {
-    int em_id;
-    char em_name[10];
-    int em_age;
-    float em_slary;
+    int id;
+    int age;
+    float salary;
+    char name[10];
 } employee_t;
 
-void employee_info(employee_t employee_1)
+void emp_info(employee_t emp)
 {
-    printf("%d\n", employee_1.em_id);
-    printf("%s\n", employee_1.em_name);
-    printf("%d\n", employee_1.em_age);
-    printf("%f\n", employee_1.em_slary);
+    printf("%d\n", emp.id);
+    printf("%d\n", emp.age);
+    printf("%f\n", emp.salary);
+    printf("%s\n", emp.name);
 }
 
 int emp_aveage_age(employee_t emp[], int n)
@@ -22,10 +22,12 @@ int emp_aveage_age(employee_t emp[], int n)
     int sum = 0;
     for (int i = 0; i < n; ++i)
     {
-        sum += emp[i].em_age;
+        sum += emp[i].age;
     }
-    return sum / n;
+    int ret = sum / n;
+    return ret;
 }
+
 void emp_writefile(employee_t emp[], int n)
 {
     FILE *fp = fopen("employee.bin", "wb+");
@@ -36,40 +38,40 @@ void emp_writefile(employee_t emp[], int n)
     fclose(fp);
     return;
 }
+
 void emp_readfile(employee_t emp[], int n)
 {
     FILE *fp = fopen("employee.bin", "rb");
     int i = 0;
-    while (fread(&emp[i], sizeof(employee_t), 1, fp))
+    while (i < n && fread(&emp[i], sizeof(employee_t), 1, fp))
     {
-        printf("[%d] %d %s\n", i, emp[i].em_id, emp[i].em_name);
-        ++i;
+        emp_info(emp[i]);
     }
+    printf("\n");
     fclose(fp);
     return;
 }
+
 int main()
 {
     employee_t emp[3];
-    emp[0].em_id = 1;
-    emp[0].em_age = 20;
-    emp[0].em_slary = 30000.0;
-    strcpy(emp[0].em_name, "IU");
-    emp[1].em_id = 2;
-    emp[1].em_age = 26;
-    emp[1].em_slary = 36000.0;
-    strcpy(emp[1].em_name, "taylor");
-    emp[2].em_id = 3;
-    emp[2].em_age = 31;
-    emp[2].em_slary = 90000.0;
-    strcpy(emp[2].em_name, "swift");
+    emp[0].id = 1;
+    emp[0].age = 20;
+    emp[0].salary = 30000.0;
+    strcpy(emp[0].name, "IU");
+    emp[1].id = 2;
+    emp[1].age = 26;
+    emp[1].salary = 36000.0;
+    strcpy(emp[1].name, "taylor");
+    emp[2].id = 3;
+    emp[2].age = 31;
+    emp[2].salary = 90000.0;
+    strcpy(emp[2].name, "swift");
     emp_writefile(emp, 3);
-    emp_readfile(emp,3);
-    for (int i = 0; i < 3; i++)
-    {
-        employee_info(emp[i]);
-    }
+    employee_t read_emp[3];
+    emp_readfile(read_emp, 3);
     int ret = emp_aveage_age(emp, 3);
+    
     printf("%d\n", ret);
     return 0;
 }
